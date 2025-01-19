f9aaaea855a039ee64e4b44a5821b94f0d7e69a0
Nadin Novoselova <nadja.novoselova@gmail.com>
2025-01-20 00:38:14 +0300

    fix: fix error of 1 part

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -5,9 +5,9 @@ const filePath = path.join(__dirname, 'text.txt');
 const readStream = fs.createReadStream(filePath);
 
 readStream.on('data', (chunk) => {
-    console.log(chunk.toString());
+  console.log(chunk.toString());
 });
 
 readStream.on('error', (error) => {
-    console.error('Sorry: ', error);
-  });
+  console.error('Sorry: ', error);
+});
